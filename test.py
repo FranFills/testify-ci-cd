b9f1d76260899ef096946749916d11eb530ff1fe
Fisayo Obabire <frannfisayo@gmail.com>
2022-09-23 13:50:45 +0100

    unit test correction

--- test.py
+++ test.py
@@ -8,9 +8,8 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.addition(5,5), 10, "Should be 10")
         self.assertEqual(main.addition(40,20), 60, "Should be 60")
         self.assertEqual(main.addition(-3,2), -1, "Should be -1")
-        self.assertEqual(main.addition(-8,5), -3, "Should be -3")
 
-if _name_ -- '_main_':
+if __name__ == '__main__':
     unittest.main()
 
 #always save your work
